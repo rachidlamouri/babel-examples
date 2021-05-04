3bf144946b137a806f4ec1bd90fdefb5d6ef6f60
Rachid Lamouri <rachidlamouri@gmail.com>
2021-05-04 07:53:20 -0400

    Add simple example

---
+++ simple-input/index.js
@@ -0,0 +1,13 @@
+const potato = 123;
+const banana = 456;
+
+console.log('Potato:', potato);
+console.log('Banana:', banana);
+
+const snackTime = () => (
+  Math.pow(2, 2)
+  + potato
+  - banana
+);
+
+console.log('Snack Time:', snackTime());
